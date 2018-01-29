13f5029a1ddf5e5f875b63a84b58a434cc47241c
Kent Fiala <Kent.Fiala@gmail.com>
2018-01-29 09:57:58 -0500

    AviSys Update 2016 to 2017
    Signed-off-by: Kent Fiala <Kent.Fiala@gmail.com>

--- upload_form.php
+++ upload_form.php
@@ -14,7 +14,7 @@ function upload_form()
 
 	$heredoc = <<<HEREDOC
 <p style="color:red;"><strong>Note:</strong>
-To be current with eBird, install the <a href="http://avisys.faintlake.com/update/">2016 Taxonomy Update for AviSys</a>.
+To be current with eBird, install the <a href="http://avisys.faintlake.com/update/">2017 Taxonomy Update for AviSys</a>.
 </p>
 <form enctype="multipart/form-data" method="POST" action="$myself" name="upform" style="max-width:40em;float:left;">
 <fieldset>
